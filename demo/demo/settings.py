e56d55313c1e7ea3b63305091242d9ff9f110c60
mishassefa <bedatuassefa@gmail.com>
2023-06-24 01:23:16 +0300

    migrate data

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog', # new
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
